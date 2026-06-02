c19e77aa52026cf86b0f2856ba7aaada9fcd09bd
Kelvin W. B. Santos <145406898+kelvinwbsantos@users.noreply.github.com>
2026-06-01 22:03:41 -0300

    feat: update wagmi configuration to include Sepolia chain and improve transport settings

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,18 +1,22 @@
-import { createConfig, http, webSocket, fallback } from 'wagmi'
-import { localhost } from 'wagmi/chains'
+import { createConfig, http, webSocket, fallback } from "wagmi";
+import { localhost, sepolia } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [localhost],
+  chains: [sepolia, localhost],
   transports: {
+    [sepolia.id]: fallback([
+      webSocket(import.meta.env.VITE_SEPOLIA_RPC_WS_URL!),
+      http(import.meta.env.VITE_SEPOLIA_RPC_URL!),
+    ]),
     [localhost.id]: fallback([
-      webSocket('ws://127.0.0.1:8545'), 
-      http('http://127.0.0.1:8545')
+      webSocket("ws://127.0.0.1:8545"),
+      http("http://127.0.0.1:8545"),
     ]),
   },
-})
+});
 
-declare module 'wagmi' {
+declare module "wagmi" {
   interface Register {
-    config: typeof config
+    config: typeof config;
   }
 }
